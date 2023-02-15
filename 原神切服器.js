e6d8e8c2451addf6b697ef7947bb18589a1dc728
Anye <anye1844@qq.com>
2023-02-15 19:56:50 +0800

    update

--- 原神切服器.js
+++ 原神切服器.js
@@ -1,6 +1,38 @@
 "ui";
-$shell.setDefaultOptions({ adb: true })
-var finish = "";
+importClass(java.io.File);
+let saf = require('./saf.js');
+if (device.sdkInt >= 30) {
+    if (!saf.hasPermission(context)) {
+        dialogs.alert('提示', '从安卓11开始，谷歌限制了Android/data目录的访问权限，' +
+            '普通应用无法直接使用java.io访问该目录的文件，' +
+            '但是可以通过安卓提供的SAF框架读取文件，需要跳转页面授权').then(() => {
+                saf.getPermission(activity);
+            })
+    }
+}
+
+
+const dataDir = saf.get_data_documentFile(context);
+
+var hasapp = []
+var hasname = []
+if (app.getAppName("com.miHoYo.Yuanshen") == "原神") {
+    hasapp.push("com.miHoYo.Yuanshen")
+    hasname.push("国服")
+}
+if (app.getAppName("com.miHoYo.ys.bilibili") == "原神") {
+    hasapp.push("com.miHoYo.ys.bilibili")
+    hasname.push("B服")
+}
+if (app.getAppName("com.miHoYo.ys.mi") == "原神") {
+    hasapp.push("com.miHoYo.ys.mi")
+    hasname.push("小米服")
+}
+if (app.getAppName("com.miHoYo.GenshinImpact") == "原神") {
+    hasapp.push("com.miHoYo.GenshinImpact")
+    hasname.push("国际服")
+}
+
 ui.layout(
     <frame>
         <vertical>
@@ -11,72 +43,44 @@ ui.layout(
                 使用前需注意：
             </text>
             <text textSize="18sp" textColor="#000000" margin="20" >
-                1.本机须有你所想切换的服务器的客户端。
+                1.请选择您当前完整安装的游戏客户端，如果您的手机上有多个完整的游戏客户端，那么请随意选择一个。“完整”的游戏客户端是指您可以正常进入游戏的客户端。
             </text>
+            <text textSize="18sp" textColor="#ff0000" margin="20" >
+                请务必确保您选择的客户端是完整的，否则会导致游戏数据丢失。
+            </text>
+            <spinner id="old" />
+
             <text textSize="18sp" textColor="#000000" margin="20" >
-                2.本切换器将删除掉其余的游戏数据，只保留一份完整的游戏数据，所以请确保至少有一个客户端是可以正常进入游戏的。
+                2.本切换器将删除掉其余的游戏数据，只保留一份完整的游戏数据。请选择您想要切换的客户端：
             </text>
-
-            <button id="YuanShen" text="切换到官服" />
-            <button id="Bilibili" text="切换到B服" />
-            <button id="GenshinImpact" text="切换到国际服" />
-
+            <spinner id="new" />
+            <button id="ok">确定</button>
         </vertical>
     </frame>
 );
 
+$ui.old.attr('entries', hasname.join('|'))
+$ui.new.attr('entries', hasname.join('|'))
 
-ui.YuanShen.click(function () {
-    var GF = shell("du -sh /sdcard/Android/data/com.miHoYo.Yuanshen").result;
-    var BF = shell("du -sh /sdcard/Android/data/com.miHoYo.ys.bilibili").result;
-    var GJ = shell("du -sh /sdcard/Android/data/com.miHoYo.GenshinImpact").result;
+ui.ok.on("click", () => {
+    var i = ui.old.getSelectedItemPosition();
+    var j = ui.new.getSelectedItemPosition();
+    if (i == j) {
+        toast("请选择不同的客户端");
+        return;
+    }
+    var oldapp = hasapp[i];
+    var newapp = hasapp[j];
 
-    if (GF[2] == "M") { shell("pm clear com.miHoYo.Yuanshen"); shell("rm /sdcard/Android/data/com.miHoYo.Yuanshen -r"); }
-    if (GF[2] == "G") finish = "com.miHoYo.Yuanshen";
-
-    if (BF[2] == "M") { shell("pm clear com.miHoYo.ys.bilibili"); shell("rm /sdcard/Android/data/com.miHoYo.ys.bilibili -r"); }
-    if (BF[2] == "G") finish = "com.miHoYo.ys.bilibili";
-
-    if (GJ[2] == "M") { shell("pm clear com.miHoYo.GenshinImpact"); shell("rm /sdcard/Android/data/com.miHoYo.GenshinImpact -r"); }
-    if (GJ[2] == "G") finish = "com.miHoYo.GenshinImpact";
-
-    shell("rename /sdcard/Android/data/" + finish + " /sdcard/Android/data/com.miHoYo.Yuanshen");
-    launch("com.miHoYo.Yuanshen")
-    toast("已切换到官服");
-});
-ui.Bilibili.click(function () {
-    var GF = shell("du -sh /sdcard/Android/data/com.miHoYo.Yuanshen").result;
-    var BF = shell("du -sh /sdcard/Android/data/com.miHoYo.ys.bilibili").result;
-    var GJ = shell("du -sh /sdcard/Android/data/com.miHoYo.GenshinImpact").result;
-
-    if (GF[2] == "M") { shell("pm clear com.miHoYo.Yuanshen"); shell("rm /sdcard/Android/data/com.miHoYo.Yuanshen -r"); }
-    if (GF[2] == "G") finish = "com.miHoYo.Yuanshen";
-
-    if (BF[2] == "M") { shell("pm clear com.miHoYo.ys.bilibili"); shell("rm /sdcard/Android/data/com.miHoYo.ys.bilibili -r"); }
-    if (BF[2] == "G") finish = "com.miHoYo.ys.bilibili";
-
-    if (GJ[2] == "M") { shell("pm clear com.miHoYo.GenshinImpact"); shell("rm /sdcard/Android/data/com.miHoYo.GenshinImpact -r"); }
-    if (GJ[2] == "G") finish = "com.miHoYo.GenshinImpact";
-
-    shell("rename /sdcard/Android/data/" + finish + " /sdcard/Android/data/com.miHoYo.ys.bilibili");
-    launch("com.miHoYo.ys.bilibili")
-    toast("已切换到B服");
-});
-ui.GenshinImpact.click(function () {
-    var GF = shell("du -sh /sdcard/Android/data/com.miHoYo.Yuanshen").result;
-    var BF = shell("du -sh /sdcard/Android/data/com.miHoYo.ys.bilibili").result;
-    var GJ = shell("du -sh /sdcard/Android/data/com.miHoYo.GenshinImpact").result;
-
-    if (GF[2] == "M") { shell("pm clear com.miHoYo.Yuanshen"); shell("rm /sdcard/Android/data/com.miHoYo.Yuanshen -r"); }
-    if (GF[2] == "G") finish = "com.miHoYo.Yuanshen";
-
-    if (BF[2] == "M") { shell("pm clear com.miHoYo.ys.bilibili"); shell("rm /sdcard/Android/data/com.miHoYo.ys.bilibili -r"); }
-    if (BF[2] == "G") finish = "com.miHoYo.ys.bilibili";
-
-    if (GJ[2] == "M") { shell("pm clear com.miHoYo.GenshinImpact"); shell("rm /sdcard/Android/data/com.miHoYo.GenshinImpact -r"); }
-    if (GJ[2] == "G") finish = "com.miHoYo.GenshinImpact";
-
-    shell("rename /sdcard/Android/data/" + finish + " /sdcard/Android/data/com.miHoYo.GenshinImpact");
-    launch("com.miHoYo.GenshinImpact")
-    toast("已切换到国际服");
+    for (var k = 0; k < hasapp.length; k++) {
+        if (k != i) {
+            const delDir = dataDir.findFile(hasapp[k]);
+            if (delDir != null)
+                delDir.delete();
+        }
+    }
+    const oldDir = dataDir.findFile(oldapp);
+    const newDir = oldDir.renameTo(newapp);
+    launch(hasapp[j])
+    toast("已切换为" + hasname[j]);
 });
